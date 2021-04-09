2a63ed22b72379cf04662c13e1ff0a42cdbba2c4
gg294 <Mohammed.Amine.Ouasmine@clearstream.com>
2021-04-09 14:52:06 +0200

    Initial commit

---
+++ backend/src/main/java/com/osm/backend/SpringVueBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.osm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author gg294 - 4/9/2021
+ */
+@SpringBootApplication
+public class SpringVueBackEndApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run( SpringVueBackEndApplication.class, args );
+    }
+}
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <h1>Welcome to {{ appName }}</h1>
+    <video autoplay="true" id="videoElement">
+
+    </video>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data: function() {
+      return {
+        appName: "Streamii"
+      }
+    },
+    mounted () {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+          .then(mediaStream => {
+            this.$refs.video.srcObject = mediaStream
+            this.$refs.video.play()
+          })
+          .catch(error => console.error('getUserMedia() error:', error))
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
